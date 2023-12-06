46a620a8ffac365ecf66e3f1515b3439d28a908c
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2023-12-06 17:40:29 -0500

    get restaurants by id

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
+const uuid = require('uuid')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -20,12 +21,22 @@ app.get('/restaurants', function(req,res) {
     
     const fileData = fs.readFileSync(filePath);
     const storedRestaurants = JSON.parse(fileData);
-
+    
     res.render('restaurants', { numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants });
 })
 app.get('/restaurants/:id', function (req, res) {
- const restaurantId = req.params.id;
- res.render('restaurant-detail', {rid: restaurantId});
+    const restaurantId = req.params.id;
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+    
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+    for (const restaurant of storedRestaurants) {
+        if (restaurant.id === restaurantId) {
+            return res.render('restaurant-detail', { restaurant: restaurant})
+        }
+    }
+ 
 });
 
 app.get('/about', function(req,res) {
@@ -40,6 +51,7 @@ app.get('/recommend', function(req,res) {
 
 app.post('/recommend', function(req, res) {
     const restaurant= req.body;
+    restaurant.id = uuid.v4();
     const filePath = path.join(__dirname, 'data', 'restaurants.json');
 
     const fileData = fs.readFileSync(filePath);
