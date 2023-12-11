c7062765bb3c1dd1c3211b51aa1ce360004e5d0e
Hector Biminchumo <hector.biminchumo@externos-cl.cencosud.com>
2023-12-10 19:34:27 -0500

    using query parameters to order

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -6,9 +6,26 @@ const resData = require('../util/restaurant-data');
 const router = express.Router();
 
 router.get('/restaurants', function(req,res) {
-    const storedRestaurants = resData.getStoredRestaurants();
+    let order = req.query.order;
+    let nextOrder = 'desc'
+
+    if (order !== 'asc' && order !== 'desc') {
+        order = 'asc';
+    }
+    if (order === 'desc') {
+        nextOrder = 'asc';
+    }
     
-    res.render('restaurants', { numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants });
+    const storedRestaurants = resData.getStoredRestaurants();
+
+    storedRestaurants.sort(function(resA, resB) {
+        if ((order === 'asc' && resA.name > resB.name ) || (order === 'desc' && resB.name > resA.name)) {
+            return 1;
+        }
+        return -1
+    });    
+    
+    res.render('restaurants', { numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants, nextOrder: nextOrder });
 })
 router.get('/restaurants/:id', function (req, res) {
     const restaurantId = req.params.id;
